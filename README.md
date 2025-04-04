f93fbc9d2386a54cb784d6a47cdeb0327e94a1a1
Vadim Kondratev <vadim@arangodb.com>
2025-04-04 14:21:32 +0200

    Release rclone with golang-1.22.12 on 04.04.2025

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # rclone-arangodb
+
+This repository was made to deliver recompiled rclone versions included into
+ArangoDB <MAJOR>.<MINOR> branches releases.
+
+Each <MAJOR>.<MINOR> branch tied to the specific rclone version which cannot be
+changed over time to remain backwards compatibility. However in case of any
+additional CVEs there is a need to rebuild it with newer toolchain (either by
+raising golang version or updating dependent components).
+
+## How to make a release
+
+### Prerequisites
+
+In order to make a release with newer toolchain the following prerequisites are
+required:
+ - `git` latest stable version
+ - `docker` latest stable version
+
+### Steps
+
+- create a commit with necessary changes if needed:
+  - set `golang` version within `./toolchain.env`
+  - check `<MAJOR>.<MINOR>` used rclone version in `rclone.env` and produced
+    targets within `targets.env`
+  - update `./make_release.sh` script in case dependant components are rclone
+    must be upgraded
+- run `./make_release.sh` to produce necessary binaries:
+  - `golang-<GO_VERSION>-<COMMIT>` folder with necessary structure and binaries
+    should be produced
+- push local changes to GitHub
+- make tagged release `golang-<GO_VERSION>-<COMMIT>` with artifacts and
+  structure of the produced local folder of the tag's name
---
+++ make_release.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd ${SCRIPT_DIR}
+. ./toolchain.env
+
+printf "Making release of rclone for ArangoDB with $GO_VERSION...\n\n"
+
+RELEASE_TAG="${GO_VERSION//:/-}-$(git rev-parse --short HEAD)"
+RELEASE_OUTPUT=${SCRIPT_DIR}/${RELEASE_TAG}
+rm -rf ${RELEASE_OUTPUT} && mkdir -p ${RELEASE_OUTPUT}
+
+for ARANGODB in $(echo 3.*)
+do
+  cd ${SCRIPT_DIR}/${ARANGODB}
+
+  . ./rclone.env
+
+  printf "Making release of rclone for ArangoDB $ARANGODB...\n\n"
+
+  . ./targets.env
+  rm -rf ./rclone && \
+  git clone --single-branch --depth 1 -b ${RCLONE_VERSION} https://github.com/rclone/rclone.git && cd ./rclone
+
+  for TARGET in ${TARGETS}
+  do
+    TARGET_OS="${TARGET%_*}"
+    TARGET_ARCH="${TARGET#*_}"
+    printf "Building rclone $RCLONE_VERSION for $TARGET_OS-$TARGET_ARCH...\n\n"continue
+    docker run --rm -v "$(pwd):/rclone" \
+      -v "${HOME}:/user" \
+      -e "GOPATH=/user/rclone_go/go" \
+      -e "GOCACHE=/user/rclone_go/cache" \
+      -e "RCLONE_OUTPUT=${RELEASE_TAG}_${ARANGODB}_${RCLONE_VERSION}" \
+      -e "TARGET_OS=$TARGET_OS" \
+      -e "TARGET_ARCH=$TARGET_ARCH" \
+      -u "${UID}:${GID}" \
+      -w /rclone ${GO_VERSION} \
+      bash +x -c 'cd /rclone; go mod vendor; go get -u github.com/cloudflare/circl; go get -u github.com/Azure/azure-sdk-for-go/sdk/azidentity; go get -u golang.org/x/crypto; go get google.golang.org/api; go mod tidy; go mod vendor; function build { GOOS=$1 GOARCH=$2 CGO_ENABLED=0 go build -trimpath -ldflags="-s -w -X github.com/rclone/rclone/fs.VersionSuffix= " -tags cmount -o /rclone/${RCLONE_OUTPUT}_rclone-arangodb-$(echo $1 | sed "s/darwin/macos/g")-$2$([[ $1 == "windows" ]] && echo ".exe"); }; go get -u golang.org/x/sys; build $TARGET_OS $TARGET_ARCH' && \
+      mv ./*rclone-arangodb-*-* "${RELEASE_OUTPUT}" && rm -rf ./*rclone-arangodb-*-*
+    printf "\n\nDone building rclone $RCLONE_VERSION for $TARGET_OS-$TARGET_ARCH!\n\n"
+  done
+  printf "\n\nDone making release of rclone for ArangoDB $ARANGODB!\n\n"
+  rm -rf ${SCRIPT_DIR}/${ARANGODB}/rclone
+done
+
+printf "\n\nDone making release of rclone for ArangoDB with $GO_VERSION!\n\n"
